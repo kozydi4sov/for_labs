34b7740a444f61a204dd8a9923a76b170e3f6b4a
Ерсултан Козы-Диас <ersultankozydias@MacBook-Air-Ersultan.local>
2024-02-18 21:33:06 +0600

    3

--- Lab3/Function2/exercise1.py
+++ Lab3/Function2/exercise1.py
@@ -1,7 +1,7 @@
-def rate(x, num):
+def rate(x, b):
     for dictionary in movies:
         for key in dictionary:
             if dictionary.get("name", "movie not found") == x:
-               if dictionary["imdb"] > num:
+               if dictionary["imdb"] > b:
                    return True
     return False
