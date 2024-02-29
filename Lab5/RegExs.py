25e185d9d5346cd3625b285c406527af20dc72fa
Ерсултан Козы-Диас <ersultankozydias@MacBook-Air-Ersultan.local>
2024-02-29 06:23:13 +0600

    5

--- Lab5/RegExs.py
+++ Lab5/RegExs.py
@@ -31,3 +31,41 @@ import re
 txt = input()
 x = re.findall("a.+b", txt)
 print(x)
+
+#6  
+
+import re
+x = input()
+txt = re.sub("[ ,.]",":",x)
+print(txt)
+
+#7
+
+import re
+txt = input()
+str = txt.split('_')
+x = str[0] + ''.join(i.title() for i in str[1:])
+print(x)
+
+#8
+
+import re
+txt = input()
+x = re.findall("[A-Z][^A-Z]*",txt)
+print(x)
+
+#9
+
+import re
+txt = input()
+x = re.findall("[A-Z][^A-Z]*",txt)
+slova = ' '.join(x)
+print(slova)
+
+#10
+
+import re
+txt = input()
+x = re.findall(r"[A-Z][a-z0-9]*",txt)
+slova = '_'.join(i.lower() for i in x)
+print(slova)
